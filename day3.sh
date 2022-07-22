291d1f68e2b4a2ce50a10c172cabf7a843345873
madhusolanki01 <109496925+madhusolanki01@users.noreply.github.com>
2022-07-22 21:51:37 +0530

    Add files via upload

---
+++ day3.sh
@@ -0,0 +1,13 @@
+
+
+for x in `ls *.py`
+do
+filename=`echo $x | awk -F. '{ print $1 }'`
+if [ -d $filename ]
+then
+echo "Inside If Condition Works"
+rm -rf $filename
+fi
+mkdir $filename
+cp -r $x $filename
+done
