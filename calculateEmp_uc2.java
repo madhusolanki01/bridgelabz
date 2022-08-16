9ed177e142652b8e222683366dc45255031bbcff
madhusolanki01 <109496925+madhusolanki01@users.noreply.github.com>
2022-08-17 00:39:33 +0530

    Add files via upload

---
+++ calculateEmp_uc2.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class calculateEmp_uc2 {
+
+	public static void main(String args[])
+	{
+		int Wage_Per_Hour =20;
+		int full_day_hour =8;
+		double calculatewage =  Wage_Per_Hour * full_day_hour;
+		System.out.println("Total daily wage is: " + calculatewage);
+	}
+}
